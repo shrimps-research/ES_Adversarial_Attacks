2e076a8a59fe9415794782043bea303b8be51f10
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-03-17 18:30:07 +0100

    progress on mutation

---
+++ classes/Mutation.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from classes.Population import Population
+from classes.Individual import Individual
+
+class Mutation:
+    def mutate(self, individual: Individual) -> np.ndarray:
+        pass
+
+
+class CustomSigma(Mutation):
+
+    def __init__(self, lr, lr_prime):
+        self.lr = lr
+        self.lr_prime = lr_prime  
+
+    def mutate(self, individual: Individual):
+        for curr_sig in range(individual.n_values):
+            # Update current sigma
+            normal_matr_prime = np.random.normal(0,self.lr_prime,1)
+            normal_matr = np.random.normal(0,self.lr,1)
+            individual.sigmas[curr_sig] = individual.sigmas[curr_sig]*(
+                                    np.exp(normal_matr+normal_matr_prime))
+
+            sigma_noise = np.random.normal(0,individual.sigmas[curr_sig],1)
+            individual.values[curr_sig] = individual.values[curr_sig] + sigma_noise
+        
+        
--- classes/Population.py
+++ classes/Population.py
@@ -3,21 +3,32 @@ import numpy as np
 
 class Population:
 
-    def __init__(self, size, n_values, is_min):
+    def __init__(self, size, n_values, is_min, evaluation_function):
+        """
+        size: size of population
+        n_values: size of value array of individual
+        is_min: if != 0 then switch from maximization to minimization problem
+        evaluation_function: function that returns the evaluation for an individual
+        """
+
         self.size = size
         self.is_min = is_min
         if is_min:
             self.individuals = [Individual(n_values, np.inf) for i in range(size)]
         else:
             self.individuals = [Individual(n_values, np.NINF) for i in range(size)]
+        self.eval_fun = evaluation_function
+
 
     def ave_fitness(self):
         return sum(individual.fitness for individual in self.individuals) / len(self.individuals)
+
 
     def max_fitness(self, get_index=False):
         if get_index:
             return np.argmax([ individual.fitness for individual in self.individuals])
         return max(individual.fitness for individual in self.individuals)
+
 
     def min_fitness(self,get_index=False):
         if get_index:
@@ -35,5 +46,11 @@ class Population:
                 return self.max_fitness(get_index=True)
             return self.max_fitness()
 
+
     def best_individual(self):
         return self.individuals[self.best_fitness(get_index=True)]
+
+
+    def evaluate_fitness(self):
+        for individual in self.individuals:
+            individual.fitness = self.eval_fun(individual.values)
