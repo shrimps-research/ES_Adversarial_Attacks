206457623598c63b1fc7e2d0f3135747ce1b2048
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-03-19 02:40:55 +0100

    fixed model

--- tf_model/SimpleClassifier.py
+++ tf_model/SimpleClassifier.py
@@ -5,8 +5,9 @@ from tensorflow.keras.models import Sequential
 
 class SimpleClassifier:
 
-    def __init__(self):
-        
+    def __init__(self,model_path):
+
+        self.model_path = model_path
         self.batch_size = 32
         self.img_height = 180
         self.img_width = 180
@@ -28,21 +29,4 @@ class SimpleClassifier:
         )
 
 
-        self.model = Sequential([
-                            self.data_augmentation,
-                            layers.Rescaling(1./255),
-                            layers.Conv2D(16, 3, padding='same', activation='relu'),
-                            layers.MaxPooling2D(),
-                            layers.Conv2D(32, 3, padding='same', activation='relu'),
-                            layers.MaxPooling2D(),
-                            layers.Conv2D(64, 3, padding='same', activation='relu'),
-                            layers.MaxPooling2D(),
-                            layers.Dropout(0.2),
-                            layers.Flatten(),
-                            layers.Dense(128, activation='relu'),
-                            layers.Dense(self.num_classes)
-                            ])
-        self.model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-        self.model.load_weights('tf_model/model_weights/checkpoint.ckpt')
+        self.model = tf.keras.models.load_model(self.model_path)
