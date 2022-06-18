29b6b8efe6cf70b6e9a6d5482d190d5fd789a058
riccardomajellaro <riccardomajellaro@gmail.com>
2022-06-18 16:47:10 +0200

    minor fix

--- testing/attack_dir.py
+++ testing/attack_dir.py
@@ -6,7 +6,7 @@ src_dir = "../data/img_data/imagenet/komodo"
 dst_dir = "../data/img_data/test"
 for img_name in os.listdir(src_dir):
     os.replace(f"{src_dir}/{img_name}", f"{dst_dir}/{img_name}")
-    attack = subprocess.run(["python", "attack_main.py", "-model", "xception_classifier", "-device", "cuda", "-eval", "crossentropy", "-in", "../data/img_data/test/", "-tl", "48", "-min", "-b", "1000", "-ps", "12", "-os", "50", "-d", "0.9", "-e", "0.05", "-r", "global_discrete", "-m", "individual", "-s", "comma_selection", "-fp", "5", "-v", "2"], capture_output=True, text=True)
+    attack = subprocess.run(["python", "attack_main.py", "-model", "xception_classifier", "-device", "cuda", "-eval", "crossentropy", "-in", dst_dir+"/", "-tl", "48", "-min", "-b", "1000", "-ps", "12", "-os", "50", "-d", "0.9", "-e", "0.05", "-r", "global_discrete", "-m", "individual", "-s", "comma_selection", "-fp", "5", "-v", "2"], capture_output=True, text=True)
     output = attack.stdout[attack.stdout.find("Best function evaluation:"):]
     with open("noisy_acc_komodo_single.txt", "a") as res_file:
         res_file.write(f"{img_name}:\n{output}\n")
